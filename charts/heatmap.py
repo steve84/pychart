7681fbb1340915efbc4451dea7137813684f30cb
Steve Aschwanden <steve.aschwanden@bluewin.ch>
2021-03-20 07:31:37 +0100

    Normalize heatmap values -1,1

--- charts/heatmap.py
+++ charts/heatmap.py
@@ -4,15 +4,24 @@ import matplotlib.pyplot as plt
 
 
 def createHeatmap(
-        data, labels, filename, xtick_rotation=45,
-        xtick_position='right', hscaling=1, vscaling=1):
+        data, labels, filename, title=None,
+        xtick_rotation=45, xtick_position='right',
+        hscaling=1, vscaling=1):
 
     mask = np.triu(np.ones_like(data, dtype=np.bool), 1)
 
     fig, axes = plt.subplots(1, 1, squeeze=False)
     g = sb.heatmap(
         data, mask=mask, annot=True, fmt=".2f", linewidths=.5,
-        xticklabels=labels, yticklabels=labels)
+        xticklabels=labels, yticklabels=labels,
+        cmap='RdYlGn', vmin=-1.0, vmax=1.0)
+
+    cbar = g.collections[0].colorbar
+    cbar.set_ticks([-1, -.75, -.5, -.25, 0, .25, .5, .75, 1])
+    cbar.set_ticklabels(
+        ['-1.0', '-0.75', '-0.5', '-0.25', '0',
+        '0.25', '0.5', '0.75', '1.0']
+    )
 
     g.set_xticklabels(
         g.get_xticklabels(), rotation=xtick_rotation,
@@ -25,5 +34,6 @@ def createHeatmap(
         ])
     )
 
+    plt.title(title)
     plt.tight_layout()
     plt.savefig(filename, format='png')
